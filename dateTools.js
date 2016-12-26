b1547e9e4b411d7cf8020d2380696463c0a7db3b
Andrew Fischer <afischer15@mac.com>
2016-12-26 18:06:13 -0500

    ESLint config, new date tools

--- dateTools.js
+++ dateTools.js
@@ -4,10 +4,34 @@ var getYear = function() {
 
 var getMonth = function() {
   var date = new Date();
-  return ('0' + (date.getMonth() + 1)).slice(-2);
+  return date.getMonth() + 1;
+}
+
+var get2ndLvlPagePaths = function() {
+  var thisYear = getYear();
+
+  if (getMonth() === 1) {
+    return `ga:pagePathLevel2==/${thisYear}/,ga:pagePathLevel2==/${thisYear - 1}/`
+  }
+  return `ga:pagePathLevel2==/${thisYear}/`
+}
+
+var get3rdLvlPagePaths = function() {
+  var date = new Date();
+  var lastMonth;
+  var thisMonth;
+
+  if (getMonth() === 1) { // If Jan, get Dec of last year too
+    lastMonth = '12';
+    thisMonth = '01';
+  } else {
+    lastMonth = ('0' + (date.getMonth())).slice(-2);
+    thisMonth = ('0' + (date.getMonth() + 1)).slice(-2);
+  }
+  return `ga:pagePathLevel3==/${lastMonth}/,ga:pagePathLevel3==/${thisMonth}/`
 }
 
 module.exports = {
-  getYear: getYear,
-  getMonth: getMonth
+  get2ndLvlPagePaths: get2ndLvlPagePaths,
+  get3rdLvlPagePaths: get3rdLvlPagePaths
 }
