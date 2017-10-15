762084d3934db386939ef78a9e1fb52701b8f92f
Andrew Fischer <afischer15@mac.com>
2017-10-15 18:59:20 -0400

    use google default credential auth, more gracefully handle errors

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Running Locally
-`npm start`
+# Dropcap
 
+## Local Development
+1. Get the dropcap service credentials JSON file. Rename it to `dropcap-service-credentials.json`
+and put it in this folder
+2. set the `GOOGLE_APPLICATION_CREDENTIALS` environment variable to the path of this file. for example,
+`export GOOGLE_APPLICATION_CREDENTIALS=/Users/dpdev/Documents/dropcap/dropcap-service-credentials.json`
+3. Log in to your google account `gcloud auth application-default login`
+4. Run the express server using `npm start`
 
-# Deploying to GCP
+## Deploying to GCP
 1. If you have not done so already, Install `gcloud` command line tool
 (via `curl https://sdk.cloud.google.com | bash` and run `gcloud init`
   within this folder
--- app.js
+++ app.js
@@ -6,43 +6,59 @@ const openGraph  = require('open-graph-scraper')
 const util       = require('./util')
 const constants  = require('./constants')
 
-var key = {
-  'type': 'service_account',
-  'project_id': 'dailypenn-web-top10',
-  'private_key_id': '6742611634d9b0871986a88b1e09101b8d014a5e',
-  'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCxrToODL1ySnSV\nE1aIncnTC+e1omuxClN4gctCJnGaSzn8Uz69jKtBZPxlcumIVvAFPp5PPzApFCWV\nYSAAoUCy7Kk4sO26ZTKc6n+GgSQ6dFeHycG/qqrkoHmBmU7k0xoN0HgxB9N53o3h\ndP+1OnOMmWKKy0atOr1tQOZ+jrPk/xKBcEK0iIDspvDtkhUgY5QZsLxdDX/WMkIZ\nWErPtrhK91cjfG/4UXzVQVIILrazpIuYQhrP+2Ki3FmS5XeqU1vakT9zLbmxbGaa\nV2GaRgtwtrYOTx6VJhHDz6PAeK8hxAWcZTed3MjdVFXejzaBolSuzLH3Gq72tj+f\n1UF901jrAgMBAAECggEAOTZNfl6t1hKRjbtHXwBoD7RCrWMaiEyJ6kDpIWjdpdBZ\nYJ2xkzmrn4gUGdqxxjwwV4/9TAlNybx6CarXsjw6aCL6ro2STzXs4UhFPhtxff0e\nflQCNmSPWMUvuwKRu+8Z8Z7SRP9/Ijv+tb2F5WZn9LsjVwvzuy4x9u8yCUiMNs0+\nXtBH6qlzcDnGQ55YqtN5nhm6BcDBFwOpSI+iEhXGluMNGUa+TUa3ugzSVrvomZ1A\naUrb/g0qWbXpAy7mJmt4jbGy3aYE/DZREX9KvHMCGi+9PX70mRZjSpB/3t8Idv2j\nedaXPixfj6SKZY94bgXYXaO9JxMV/IuvFoFm0yuyKQKBgQDWru8kv4HZp98RC1gk\nTZfh4yHZstlnqT0jrYC7vqIUwrRSYJNgSkT+zxOYP3tIIxxVuRZW7AEr5aDEkUId\nqD0utUCrVDMLaYZcBkNIuK9waFn4zQEAiUrJOzxV65JIPuH9k2rPcaiXBM6CLf1v\nn6L/ghIjHzvLspQCo08oA7LUPwKBgQDT3wo2Y72vMbI2/dEgKAnQQSsnuH8aJ+Cs\nrbO9lI7ZpxQuWsF4ijB8H1TkVSyzgr2u9/zW0OGelPRpuNEhPlJPV0qUQA/RQGwY\n62SVWSFK2DRG/MND26kOdo0i3xEin/3RWt+iBBqBKG2jI+yspk9FaSPFwBQz4YZo\nfF3CkFUgVQKBgF0dCLpOmbpWtkiFPBia0vp1cjQi/2qXm94CnehQWapZfK6SFrnX\n3K8MD7qkJgt2tw5zNb5DpIpukVlQi3Wjh83fbYoh/fXAMjdtu/cpq2Y8zHE+MWYT\nJi27lILLpunQ5GIAc35AFNhTjGIoIlksyxy5RLaZEG1cKhguzuFTIVgvAoGBAMP8\nKsc7Jki8ML2btmoHf2RsFA5vJOu9/Z/eIkj49dcyLYHfo93r/oOiu/VnMK1xACOs\n8+6GVF5MLXbqZlFZA+yus6dFRxxqelPm0ykvxJMmQd10jO/lbpGZal7ad1BPLQwP\nApUtNJNLIXfqkFD4mpV7d4LCdi2zrZBTkquatZPtAoGBAIFjY6mTPdNR3wtVZlxJ\nMN8Y41aKchJOurl4zqm+zB/A/bI2BMPMaghdmFDM9F6rFqePUdeB7UWaHL257iL9\ncVp363PubCOgnAdUw0VU363Gq2ljc7i/CEj9kdKoVNpmhvrDLb55nBQyxyIiO7kl\nD3CPlpp67tXVcuJwYvffEQvb\n-----END PRIVATE KEY-----\n',
-  'client_email': 'dp-top10@dailypenn-web-top10.iam.gserviceaccount.com',
-  'client_id': '108413780379481780591',
-  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-  'token_uri': 'https://accounts.google.com/o/oauth2/token',
-  'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-  'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/dp-top10%40dailypenn-web-top10.iam.gserviceaccount.com'
-}
+// var key = {
+//   'type': 'service_account',
+//   'project_id': 'dailypenn-web-top10',
+//   'private_key_id': '6742611634d9b0871986a88b1e09101b8d014a5e',
+//   'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCxrToODL1ySnSV\nE1aIncnTC+e1omuxClN4gctCJnGaSzn8Uz69jKtBZPxlcumIVvAFPp5PPzApFCWV\nYSAAoUCy7Kk4sO26ZTKc6n+GgSQ6dFeHycG/qqrkoHmBmU7k0xoN0HgxB9N53o3h\ndP+1OnOMmWKKy0atOr1tQOZ+jrPk/xKBcEK0iIDspvDtkhUgY5QZsLxdDX/WMkIZ\nWErPtrhK91cjfG/4UXzVQVIILrazpIuYQhrP+2Ki3FmS5XeqU1vakT9zLbmxbGaa\nV2GaRgtwtrYOTx6VJhHDz6PAeK8hxAWcZTed3MjdVFXejzaBolSuzLH3Gq72tj+f\n1UF901jrAgMBAAECggEAOTZNfl6t1hKRjbtHXwBoD7RCrWMaiEyJ6kDpIWjdpdBZ\nYJ2xkzmrn4gUGdqxxjwwV4/9TAlNybx6CarXsjw6aCL6ro2STzXs4UhFPhtxff0e\nflQCNmSPWMUvuwKRu+8Z8Z7SRP9/Ijv+tb2F5WZn9LsjVwvzuy4x9u8yCUiMNs0+\nXtBH6qlzcDnGQ55YqtN5nhm6BcDBFwOpSI+iEhXGluMNGUa+TUa3ugzSVrvomZ1A\naUrb/g0qWbXpAy7mJmt4jbGy3aYE/DZREX9KvHMCGi+9PX70mRZjSpB/3t8Idv2j\nedaXPixfj6SKZY94bgXYXaO9JxMV/IuvFoFm0yuyKQKBgQDWru8kv4HZp98RC1gk\nTZfh4yHZstlnqT0jrYC7vqIUwrRSYJNgSkT+zxOYP3tIIxxVuRZW7AEr5aDEkUId\nqD0utUCrVDMLaYZcBkNIuK9waFn4zQEAiUrJOzxV65JIPuH9k2rPcaiXBM6CLf1v\nn6L/ghIjHzvLspQCo08oA7LUPwKBgQDT3wo2Y72vMbI2/dEgKAnQQSsnuH8aJ+Cs\nrbO9lI7ZpxQuWsF4ijB8H1TkVSyzgr2u9/zW0OGelPRpuNEhPlJPV0qUQA/RQGwY\n62SVWSFK2DRG/MND26kOdo0i3xEin/3RWt+iBBqBKG2jI+yspk9FaSPFwBQz4YZo\nfF3CkFUgVQKBgF0dCLpOmbpWtkiFPBia0vp1cjQi/2qXm94CnehQWapZfK6SFrnX\n3K8MD7qkJgt2tw5zNb5DpIpukVlQi3Wjh83fbYoh/fXAMjdtu/cpq2Y8zHE+MWYT\nJi27lILLpunQ5GIAc35AFNhTjGIoIlksyxy5RLaZEG1cKhguzuFTIVgvAoGBAMP8\nKsc7Jki8ML2btmoHf2RsFA5vJOu9/Z/eIkj49dcyLYHfo93r/oOiu/VnMK1xACOs\n8+6GVF5MLXbqZlFZA+yus6dFRxxqelPm0ykvxJMmQd10jO/lbpGZal7ad1BPLQwP\nApUtNJNLIXfqkFD4mpV7d4LCdi2zrZBTkquatZPtAoGBAIFjY6mTPdNR3wtVZlxJ\nMN8Y41aKchJOurl4zqm+zB/A/bI2BMPMaghdmFDM9F6rFqePUdeB7UWaHL257iL9\ncVp363PubCOgnAdUw0VU363Gq2ljc7i/CEj9kdKoVNpmhvrDLb55nBQyxyIiO7kl\nD3CPlpp67tXVcuJwYvffEQvb\n-----END PRIVATE KEY-----\n',
+//   'client_email': 'dp-top10@dailypenn-web-top10.iam.gserviceaccount.com',
+//   'client_id': '108413780379481780591',
+//   'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+//   'token_uri': 'https://accounts.google.com/o/oauth2/token',
+//   'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+//   'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/dp-top10%40dailypenn-web-top10.iam.gserviceaccount.com'
+// }
 
-var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/analytics.readonly'], null)
+// var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/analytics.readonly'], null)
 
 function queryTopArticles(analytics, viewName, maxResults) {
   return new Promise((resolve, reject) => {
-    analytics.data.ga.get({
-      'auth': jwtClient,
-      'ids': constants.VIEW_ID[viewName],
-      'metrics': 'ga:pageViews',
-      'dimensions': 'ga:pageTitle,ga:pagePath',
-      'start-date': '7daysAgo',
-      'end-date': 'today',
-      'sort': '-ga:pageViews',
-      'max-results': maxResults * 2, // get 2x max results to remove dupes
-      'filters': 'ga:pagePathLevel1==/article/;' + util.get2ndLvlPagePaths() + ';' + util.get3rdLvlPagePaths()
-    }, function (err, response) {
+    google.auth.getApplicationDefault(function(err, authClient) {
       if (err) {
-        console.error('Analytics fetching error')
-        console.error(err)
+        console.error('Google auth error')
+        console.error(err);
         reject(err)
       }
+      console.log(viewName, '**********');
+      // set scopes to analytics readonly
+      if (authClient.createScopedRequired && authClient.createScopedRequired()){
+        authClient = authClient.createScoped(
+          ['https://www.googleapis.com/auth/analytics.readonly']
+        )
+      }
+      console.log(constants.VIEW_ID[viewName]);
+      analytics.data.ga.get({
+        'auth': authClient,
+        'ids': constants.VIEW_ID[viewName],
+        'metrics': 'ga:pageViews',
+        'dimensions': 'ga:pageTitle,ga:pagePath',
+        'start-date': '7daysAgo',
+        'end-date': 'today',
+        'sort': '-ga:pageViews',
+        'max-results': maxResults * 2, // get 2x max results to remove dupes
+        'filters': 'ga:pagePathLevel1==/article/;' + util.get2ndLvlPagePaths() + ';' + util.get3rdLvlPagePaths()
+      }, function (err, response) {
+        if (err) {
+          console.error('Analytics fetching error')
+          console.error(err)
+          reject(err)
+        }
+        var topURLs = util.combineAndStripURLs(response.rows, maxResults)
+        return resolve(urlDataAsJSON(topURLs, viewName))
+      })
+    });
 
-      var topURLs = util.combineAndStripURLs(response.rows, maxResults)
-      return resolve(urlDataAsJSON(topURLs, viewName))
-    })
+
   })
 }
 
@@ -102,20 +118,25 @@ var mergeOGData = function(canonicalURL, urlData) {
 
 function getTopTen(property) {
   return new Promise((resolve, reject) => {
-    jwtClient.authorize((err, tokens) => {
-      if (err) {
-        reject(err)
-      }
-      var analytics = google.analytics('v3')
-      resolve(queryTopArticles(analytics, property, 10))
-    })
+    var analytics = google.analytics('v3')
+    resolve(queryTopArticles(analytics, property, 10))
   })
 }
 
+app.get('/favicon.ico', function (request, res) {
+  // do nothing
+  res.status(204);
+});
+
 app.get('/:property', function (request, res) {
+  const propertyName = request.params.property
+  if (constants.VIEW_ID[propertyName] == null) {
+    res.send(`{"error": "unknown google analytics property ${propertyName}"}`)
+    return
+  }
   // 10 browser cache, 30 minute public cache
   res.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
-  getTopTen(request.params.property).then((data) => res.send(data))
+  getTopTen(propertyName).then((data) => res.send(data))
 })
 
 exports.dropcap = app
