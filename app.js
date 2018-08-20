075a01d73818b2291fdae96ca69a60282963b650
Alex Graves <graves@thedp.com>
2018-08-20 18:08:47 -0400

    add semicolons, run eslint

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express   = require('express')
-const cors      = require('cors')
-const google    = require('googleapis')
-const openGraph = require('open-graph-scraper')
+const express   = require('express');
+const cors      = require('cors');
+const google    = require('googleapis');
+const openGraph = require('open-graph-scraper');
 
-const util      = require('./util')
-const conf      = require('./config.json')
+const util      = require('./util');
+const conf      = require('./config.json');
 
+const app = express();
+app.use(cors());
 
-const app = express()
-app.use(cors())
-
-const VIEWS = conf.views
+const VIEWS = conf.views;
 
 function queryTopArticles(analytics, viewName, maxResults) {
-  const key = require('./dropcap-service-credentials.json')
+  const key = require('./dropcap-service-credentials.json');
   return new Promise((resolve, reject) => {
     var jwtClient = new google.auth.JWT(
       key.client_email,
@@ -21,12 +20,12 @@ function queryTopArticles(analytics, viewName, maxResults) {
       key.private_key,
       ['https://www.googleapis.com/auth/analytics.readonly'],
       null
-    )
+    );
 
-    jwtClient.authorize((err, tokens) => {
+    jwtClient.authorize(err => {
       if (err) {
-        console.error(err)
-        reject(err)
+        console.error(err);
+        reject(err);
       }
 
       analytics.data.ga.get({
@@ -43,58 +42,58 @@ function queryTopArticles(analytics, viewName, maxResults) {
           `ga:pagePathLevel1==/article/;${util.getYearPagePath(2)};${util.getMonthPagePath(3)}`
       }, function (err, response) {
         if (err) {
-          console.error('Analytics fetching error')
-          console.error(err)
-          reject(err)
+          console.error('Analytics fetching error');
+          console.error(err);
+          reject(err);
         }
-        var topURLs = util.combineAndStripURLs(response.rows, maxResults)
-        return resolve(urlDataAsJSON(topURLs, viewName))
-      })
-    })
-  })
+        var topURLs = util.combineAndStripURLs(response.rows, maxResults);
+        return resolve(urlDataAsJSON(topURLs, viewName));
+      });
+    });
+  });
 }
 
 var urlDataAsJSON = function(urlList, viewName) {
-  const baseURL = VIEWS[viewName].baseURL
+  const baseURL = VIEWS[viewName].baseURL;
   return new Promise((resolve, reject) => {
-    var result = []
+    var result = [];
 
     for (var item in urlList) {
-      var urlItem = urlList[item]
-      var urlPath = urlItem[1] // get URL from list item
+      var urlItem = urlList[item];
+      var urlPath = urlItem[1]; // get URL from list item
 
       // remove leading title TODO Make this a constant or something.
-      urlItem[0] = urlItem[0].replace('The Daily Pennsylvanian | ', '')
-      urlItem[0] = urlItem[0].replace(' | The Daily Pennsylvanian', '')
-      urlItem[0] = urlItem[0].replace(' | 34st Street Magazine', '')
+      urlItem[0] = urlItem[0].replace('The Daily Pennsylvanian | ', '');
+      urlItem[0] = urlItem[0].replace(' | The Daily Pennsylvanian', '');
+      urlItem[0] = urlItem[0].replace(' | 34st Street Magazine', '');
 
       mergeOGData(baseURL + urlPath, urlItem).then(function(data) {
-        result.push(data)
+        result.push(data);
 
         // Check for done conditions
         if (result.length === urlList.length) {
           result.sort(function(o1, o2) {
-            return o2.views - o1.views
-          })
-          result = {'result': result}
-          return resolve(result)
+            return o2.views - o1.views;
+          });
+          result = {'result': result};
+          return resolve(result);
         }
       }, function(err) {
-        console.error('Open graph merging error')
-        console.error(err)
-        reject(err)
-      })
+        console.error('Open graph merging error');
+        console.error(err);
+        reject(err);
+      });
     }
-  })
-}
+  });
+};
 
 var mergeOGData = function(canonicalURL, urlData) {
   return new Promise((resolve, reject) =>  {
     openGraph({url: canonicalURL, timeout: 9000}, (err, results) => {
       if (err) {
-        console.error('Open graph fetching error')
-        console.error(err)
-        reject(results)
+        console.error('Open graph fetching error');
+        console.error(err);
+        reject(results);
       }
       var res = {
         'gaTitle': urlData[0].encodeHTML(),
@@ -103,34 +102,34 @@ var mergeOGData = function(canonicalURL, urlData) {
         'authors': urlData[2].split(', '),
         'views': urlData[3],
         'image': results.data.ogImage.url.replace('p.', 't.') // preview->thumb
-      }
-      resolve(res)
-    })
-  })
-}
+      };
+      resolve(res);
+    });
+  });
+};
 
 function getTopTen(property) {
   return new Promise((resolve) => {
-    var analytics = google.analytics('v3')
-    resolve(queryTopArticles(analytics, property, 10))
-  })
+    var analytics = google.analytics('v3');
+    resolve(queryTopArticles(analytics, property, 10));
+  });
 }
 
 // No favicon - just to keep it from throwing errors
-app.get('/favicon.ico', (req, res) => { res.status(204) })
+app.get('/favicon.ico', (req, res) => { res.status(204); });
 
 app.get('/:property', (req, res) => {
-  const propertyName = req.params.property.toUpperCase()
+  const propertyName = req.params.property.toUpperCase();
   if (VIEWS[propertyName].id == null) {
-    res.send(`{"error": "unknown google analytics property ${propertyName}"}`)
-    return
+    res.send(`{"error": "unknown google analytics property ${propertyName}"}`);
+    return;
   }
   // 10 min browser cache, 30 minute public cache
-  res.set('Cache-Control', 'public, max-age=600, s-maxage=1800')
-  res.set('Access-Control-Allow-Origin', '*')
-  res.set('Access-Control-Allow-Methods', 'GET')
-  res.set('Cache-Control', 'public, max-age=600, s-maxage=1800')
-  getTopTen(propertyName).then((data) => res.send(data))
-})
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET');
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
+  getTopTen(propertyName).then((data) => res.send(data));
+});
 
-exports.dropcap = app
+exports.dropcap = app;
--- util.js
+++ util.js
@@ -9,45 +9,45 @@
  */
 String.prototype.encodeHTML = function() {
   return this.replace(/[\x21-\x2b\x2f\xa0-\xff\x3A-\x40\u2013-\u2044]/g, (i) => {
-    return '&#' + i.charCodeAt(0) + ';'
-  })
-}
+    return '&#' + i.charCodeAt(0) + ';';
+  });
+};
 
 
-var getYear = () => { return new Date().getFullYear() }
-var getLastYear = () => { return new Date().getFullYear() }
-var getMonth = () => { return new Date().getMonth() + 1 }
+var getYear = () => new Date().getFullYear();
+var getLastYear = () => return new Date().getFullYear();
+var getMonth = () => new Date().getMonth() + 1;
 
 // Builds a ga:pagePath parameter with the given level and the return value of a
 // given function
 var pathFactory = function(level, intFunction) {
-  return `ga:pagePathLevel${level}==/${intFunction()}/`
-}
+  return `ga:pagePathLevel${level}==/${intFunction()}/`;
+};
 
 var getYearPagePath = function(pathLevel) {
   // if in january, return this and last year. Otherwise, return current year.
   if (getMonth() === 1) {
-    return `${pathFactory(pathLevel, getYear)},${pathFactory(pathLevel, getLastYear)}`
+    return `${pathFactory(pathLevel, getYear)},${pathFactory(pathLevel, getLastYear)}`;
   }
-  return pathFactory(pathLevel, getYear)
-}
+  return pathFactory(pathLevel, getYear);
+};
 
 var getMonthPagePath = function(pathLevel) {
-  var date = new Date()
-  var month = date.getMonth()
+  var date = new Date();
+  var month = date.getMonth();
   // if in january, last month is dec, otherwise, calculate months
-  var lastMonth = () => getMonth() === 1 ? '12' : (`0${month}`).slice(-2)
-  var thisMonth = () => getMonth() === 1 ? '01' : (`0${month + 1}`).slice(-2)
+  var lastMonth = () => getMonth() === 1 ? '12' : (`0${month}`).slice(-2);
+  var thisMonth = () => getMonth() === 1 ? '01' : (`0${month + 1}`).slice(-2);
   // return pagepath with correct level for the two months
-  return `${pathFactory(pathLevel, lastMonth)},${pathFactory(pathLevel, thisMonth)}`
-}
+  return `${pathFactory(pathLevel, lastMonth)},${pathFactory(pathLevel, thisMonth)}`;
+};
 
 var removeQueryStr = function(url) {
   if (url.includes('?')) {    // remove query string
-    return url.split('?')[0]
+    return url.split('?')[0];
   }
-  return url
-}
+  return url;
+};
 
 /**
  * Removes and returns duplciate URLs based on their slug.
@@ -57,15 +57,15 @@ var removeQueryStr = function(url) {
  */
 var combineAndStripURLs = function(urlList, maxResults) {
   // Array of all URLs in urlList, with query strings removed
-  const urls = urlList.map(item => removeQueryStr(item[1]))
+  const urls = urlList.map(item => removeQueryStr(item[1]));
   // boolean array of if URL appears at previous index
-  const isRepeat = urls.map((url, index) => urls.slice(0, index).includes(url))
+  const isRepeat = urls.map((url, index) => urls.slice(0, index).includes(url));
   // filter out repeated elements, return requested number of results
-  return urlList.filter((elt, index) => !isRepeat[index]).slice(0, maxResults)
-}
+  return urlList.filter((elt, index) => !isRepeat[index]).slice(0, maxResults);
+};
 
 module.exports = {
   getYearPagePath: getYearPagePath,
   getMonthPagePath: getMonthPagePath,
   combineAndStripURLs: combineAndStripURLs
-}
+};
